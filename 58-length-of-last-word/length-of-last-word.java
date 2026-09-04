c2c6024404f9c93a364fddc7ec95d34c489b8816
Ashutosh Kumar <ashutoshk1409@gmail.com>
2026-09-04 20:08:31 +0530

    Time: 1 ms (27.10%) | Memory: 43.2 MB (32.50%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -3,21 +3,6 @@ class Solution {
 
         String []parts=s.split(" ");
         return parts[parts.length-1].length();
-        /*
-        int count=0;
-        char ch[]=s.toCharArray();
-        for(int i=ch.length-1;i>=0;i--)
-        {
-            if(ch[i]==" ")
-            {
-                break;
-            }
-            else
-            {
-                count++;
-            }
-        }
-        return count;
-        */
+       
     }
 }
